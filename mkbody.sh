f1a846f6286eb8f879578408497e0f0e23139920
Geoff Golder <gdoteof@gmail.com>
2015-02-28 14:05:06 -0500

    debug script for checking post body (disposable)

---
+++ mkbody.sh
@@ -0,0 +1,21 @@
+usage () {
+  echo "Launch CORE OS NODE"
+  echo "==============================================="
+  echo "   YOU MUST HAVE ACCESS TOKEN AT ./.DO_TOKEN"
+  echo "==============================================="
+  echo "Usage:./launch_coreos_node droplet_name path_to_cloud_config"
+  exit 1
+}
+
+[ $# -lt 2 ] && {
+  usage
+}
+
+DROPLET_NAME=$1
+TOKEN=$(cat .DO_TOKEN)
+CLOUD_CONFIG=$(cat $2)
+
+body='{"name":"'$DROPLET_NAME'","region":"nyc3","size":"512mb","private_networking":true,"image":"coreos-stable","user_data":"'$CLOUD_CONFIG'"'
+body=$body', "ssh_keys": [25214,408466]}'
+
+echo $body
